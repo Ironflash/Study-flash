7be628d19bcecd41d2e2e70bf5ff6f8dce9e936f
Gregory <gregorylaflash@gmail.com>
2012-07-29 12:33:08 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StudyFlash::Application.config.secret_token = '0368ea5d419ba569454e7f11558410f5a677802d7cce6291e9b13318620f64425f9dd6e119ece291e283961e6bd4d74caf85e6400726b990f6daf09aa0ca15f8'
